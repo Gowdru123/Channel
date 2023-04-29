e8d84264f8a7bcecbe00fe82b688e647c7caf35e
Gowdru123 <104635641+Gowdru123@users.noreply.github.com>
2023-04-29 20:31:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
+API_ID       = 15316095
+API_HASH     = "6a293d116082b81260ac83d21f595ffa"
+BOT_TOKEN    = "6213951376:AAHoTfvnzl77cKIkLXsUW0qPYlWFH2Y5xtw"
 SESSION      = ""
-DATABASE_URI = ""
+DATABASE_URI = "mongodb+srv://jnanesh:jnanesh@cluster0.8pzxa6s.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+ADMIN        = 5047601096
+CHANNEL      = "@ROCKERSBACKUP"
