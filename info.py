f51cbe23b99207a987f8c679c4448baa9e3de069
Gowdru123 <104635641+Gowdru123@users.noreply.github.com>
2023-04-29 20:34:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,6 +3,6 @@ API_HASH     = "6a293d116082b81260ac83d21f595ffa"
 BOT_TOKEN    = "6213951376:AAHoTfvnzl77cKIkLXsUW0qPYlWFH2Y5xtw"
 SESSION      = ""
 DATABASE_URI = "mongodb+srv://jnanesh:jnanesh@cluster0.8pzxa6s.mongodb.net/?retryWrites=true&w=majority"
-LOG_CHANNEL  = -1001884373226
+LOG_CHANNEL  = -1001831347923
 ADMIN        = 5047601096
 CHANNEL      = "@ROCKERSBACKUP"
