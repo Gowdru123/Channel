d57656b319b663436707801fe878c93da02e9862
Gowdru123 <104635641+Gowdru123@users.noreply.github.com>
2023-04-29 20:38:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,5 +1,5 @@
-API_ID       = 15316095
-API_HASH     = "6a293d116082b81260ac83d21f595ffa"
+API_ID       = 28475471
+API_HASH     = "ec7f0282f30eca233b11d3521dc67107"
 BOT_TOKEN    = "6213951376:AAHoTfvnzl77cKIkLXsUW0qPYlWFH2Y5xtw"
 SESSION      = ""
 DATABASE_URI = "mongodb+srv://jnanesh:jnanesh@cluster0.8pzxa6s.mongodb.net/?retryWrites=true&w=majority"
