0f2893c1f07da353072c8c82c117d9504e83e8d9
Gowdru123 <104635641+Gowdru123@users.noreply.github.com>
2023-05-01 14:12:27 +0530

    Update script.py

--- utils/script.py
+++ utils/script.py
@@ -1,26 +1,29 @@
 class script(object):
-    START = """**Hello {} 🤟**
+    START = """**Hello {} 💎**
    
-I am **Find Post Bot**.I am best Channel Link Search Bot! 
-I Will filter your channel posts automatically and send it in your group chat when Someone search it."""
+➢ I am channel filter bot no need to add filter manually 
+➢ u can connect one group to so many channel
+➢ when someone request movie in ur group it gives movie channel link automatically 
+➢ auto delete added to avoid copyright ©
+➢ my lover 💘 @jnaneshgowdru"""
 
     HELP = """To Use me In A Group
 
 - Add me in your group & channel with all permissions. 
-- Send /verify in group & wait for It To Accept Or Directly Contact To Owner After Request @GreyMatter_Bots.
+- Send /verify in group & wait for It To Accept Or Directly Contact To Owner After Request @jnaneshgowdru.
 - After verification send /connect YourChannelID
 - Example : /connect -100xxxxxxxxxx
-- Done ✅. <b><I>Enjoy 💜❤</I></b> )
+- Done ✅. <b><I>Enjoy 💜</I></b> )
 
 Remove a Channel with - /disconnect-100xxxxxxxxxxx
 this will help you to remove a indexed channel from your group.
 
 Get connected channels list with - /connections"""
 
-    ABOUT = """Developed By @CyniteBackup
+    ABOUT = """Developed By @jnaneshgowdru
 
 ✯ Mʏ Nᴀᴍᴇ:  {}
-✯ Cʀᴇᴀᴛᴏʀ: <a href='https://t.me/CyniteOfficial'>Harman</a>
+✯ Cʀᴇᴀᴛᴏʀ: <a href='https://t.me/jnaneshgowdru>jnanesh</a>
 ✯ Lᴀɴɢᴜᴀɢᴇ: <a href='https://www.python.org'>Pʏᴛʜᴏɴ 3</a>
 ✯ DᴀᴛᴀBᴀsᴇ: <a href='https://cloud.mongodb.com/'>MᴏɴɢᴏDB</a>
 ✯ Bᴏᴛ Sᴇʀᴠᴇʀ: : <a href='https://heroku.com'>Heroku</a>"""
